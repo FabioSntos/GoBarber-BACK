888b4a68f32553f74ab0ee4c1adfca3810de0b16
fabio_santos_2001@outlook.com <57735438+FabioSntos@users.noreply.github.com>
2021-09-01 10:55:45 -0300

    fix: created_at & updated_at on CreateAppointment's migration

---
+++ src/database/migrations/1630504494470-CreateAppointments.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAppointments1630429733026 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "appointments",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "provider",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "date",
+            type: "timestamp with time zone",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("appointments");
+  }
+}
