6fcd083ea0906953a27f7b85695abdc9fed224ad
fabio <fabio_santos_2001@outlook.com>
2021-09-07 22:18:40 -0300

    feat: authentication's Middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "138f3a7d0160649042fbbfe782f36399",
+    expiresIn: "1d",
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from "typeorm";
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
+import authConfig from "../config/auth";
 
 import User from "../models/User";
 
@@ -29,9 +30,11 @@ class AuthenticateUserService {
       throw new Error("Incorrect email/password Combination");
     }
 
-    const token = sign({}, "138f3a7d0160649042fbbfe782f36399", {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn,
     });
 
     return {
